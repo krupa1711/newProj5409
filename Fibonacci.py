53dfa94562622d4a5bde1d2a06e996731d29b719
krupa patel <krupa1711@gmail.com>
2020-02-28 14:15:38 -0400

    Init commit

---
+++ Fibonacci.py
@@ -0,0 +1,37 @@
+import time
+import csv
+import random
+file = open("random_number.txt","r")
+
+line=file.readlines()
+
+timelist=[]
+
+for i in line:
+    
+    N = int(i)
+    
+    n1, n2 = 0, 1
+    count = 0
+
+    if(N<=0):
+        print("Enter a non-negative integer")
+    elif(N== 1):
+        print("Fibonacci sequence upto",N,":")
+        print(n1)
+    else:
+        #print("Fibonacci sequence:")
+        start = time.time()
+        while count < N:
+           #print(n1)
+           nth = n1 + n2
+           # update values
+           n1 = n2
+           n2 = nth
+           count += 1
+        end = time.time()
+        t = end-start
+        print("Request ID: ",random.randint(1,100),"Time: ",t,"N: ",N)
+        timelist.append(t)
+        
+
